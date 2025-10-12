20ef9a1712ae276060080004a0ab55cc5364ea1f
Aryan Bansal <aryanbansalpvt@gmail.com>
2025-10-12 21:31:00 +0530

    minor

--- src/Landing/Landing_page.jsx
+++ src/Landing/Landing_page.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Hero from './Hero';
 import Leftss from '../components/Leftss';
 import Rightss from '../components/Rightss';
-import Joining from './joining';
+import Joining from './Joining';
 import Header from '../components/Header';
 
 function Landing_page() {
