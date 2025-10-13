fceb7be44067a33fae198b3ff28981a3697eca8c
Aryan Bansal <aryanbansalpvt@gmail.com>
2025-10-13 22:19:38 +0530

    forgot

--- src/Forgot.jsx
+++ src/Forgot.jsx
@@ -5,11 +5,11 @@ function Forgot() {
     <div className="flex flex-col items-center justify-center mt-20 text-center p-6 mb-20">
       <div className="bg-white/70 backdrop-blur-lg p-10 rounded-3xl shadow-2xl max-w-2xl">
         <h1 className="text-6xl font-extrabold text-purple-800 mb-6 ">
-          😵‍💫 FORGOT PASSWORD?!
+          WHY FORGOT???😡
         </h1>
         <p className="text-2xl text-gray-600 mb-8">
           We can’t really help you with that... <br /> 
-          maybe just make a new ID 🤷‍♂️
+          maybe just make a new ID 
         </p>
         <button
           onClick={() => (window.location.href = "/register")}
