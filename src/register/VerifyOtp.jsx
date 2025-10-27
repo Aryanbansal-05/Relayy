4005445892c36103b98b3e7bfb19ad0b067d1312
Aryan Bansal <aryanbansalpvt@gmail.com>
2025-10-27 22:16:05 +0530

    OTP varificatio

--- src/register/VerifyOtp.jsx
+++ src/register/VerifyOtp.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router";
 
 export default function VerifyOtp() {
   const [otp, setOtp] = useState("");
